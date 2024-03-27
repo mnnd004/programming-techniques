8927da72b100d0f24b245726846a5ec1b926abd9
mnnd004 <nhatminhnguyenduc670@gmail.com>
2024-03-27 22:29:59 +0700

    add prob1

---
+++ IT3040_Programming_Techniques/Exercise_IT3040/P1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+float xPowery(double x, int y) {
+    if (y == 0) return 1;
+    if (y % 2 == 0) return xPowery(x, y/2) * xPowery(x, y/2);
+    return x * xPowery(x, y/2) * xPowery(x, y/2);
+}
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    float result = (y>0)? xPowery(x, y) : 1/xPowery(x, y);
+    cout << result;
+    return 0;
+}
